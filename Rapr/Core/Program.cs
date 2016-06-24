67988b76fa416ea5ccc0186969e182acbc7bf642
Teddy Zhang <lostindark@live.com>
2016-06-23 23:03:35 -0700

    Embed ObjectListView.dll into the exe.

--- Rapr/Core/Program.cs
+++ Rapr/Core/Program.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Rapr
 {
     static class Program
     {
+        private static Assembly ResolveEventHandler(Object sender, ResolveEventArgs args)
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            String resourceName = string.Format("{0}.{1}.dll", assembly.EntryPoint.DeclaringType.Namespace, new AssemblyName(args.Name).Name);
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                Byte[] assemblyData = new Byte[stream.Length];
+                stream.Read(assemblyData, 0, assemblyData.Length);
+                return Assembly.Load(assemblyData);
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveEventHandler;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DSEForm());
