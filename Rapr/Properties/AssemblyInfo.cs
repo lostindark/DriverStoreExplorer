533e726040d6793b8097c7225cd7262991958ca5
timse201 <timse201@users.noreply.github.com>
2019-06-24 19:29:59 +0200

    Product name consistency

--- Rapr/Properties/AssemblyInfo.cs
+++ Rapr/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DriverStore Explorer")]
-[assembly: AssemblyDescription("DriverStore Explorer (PNPUtil Wrapper)")]
+[assembly: AssemblyTitle("Driver Store Explorer")]
+[assembly: AssemblyDescription("Driver Store Explorer (PNPUtil Wrapper)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rapr")]
